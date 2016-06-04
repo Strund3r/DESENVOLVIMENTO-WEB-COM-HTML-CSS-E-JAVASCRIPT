86784248d33397541e811ea8daba0f5479770522
Strund3r <franzwagner.str@gmail.com>
2016-06-04 10:38:21 -0300

    Added Clicky Analytics icon

--- rodape.php
+++ rodape.php
@@ -11,14 +11,17 @@
           <li><a href="http://twitter.com/mirrorfashion">Twitter</a></li>
           <li><a href="http://plus.google.com/mirrorfashion">Google+</a></li>
         </ul>
+        
+        <div class="script">
+          </script>
+          <a title="Real Time Analytics" href="http://clicky.com/100951463"><img alt="Real Time Analytics" src="//static.getclicky.com/media/links/ba$
+          <script src="//static.getclicky.com/js" type="text/javascript"></script>
+          <script type="text/javascript">try{ clicky.init(100951463); }catch(e){}</script>
+          <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100951463ns.gif" /></p></noscript>
+        </div>
+
       </div>
     </footer>
-
-  </script>
-  <a title="Real Time Analytics" href="http://clicky.com/100951463"><img alt="Real Time Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
-  <script src="//static.getclicky.com/js" type="text/javascript"></script>
-  <script type="text/javascript">try{ clicky.init(100951463); }catch(e){}</script>
-  <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100951463ns.gif" /></p></noscript>
 
   </body>
 </html>
